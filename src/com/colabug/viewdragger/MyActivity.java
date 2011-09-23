832662aa72d7f3fb2ddd93645d81d791489c4c6b
Corey Leigh Latislaw <colabug@gmail.com>
2011-09-23 09:44:45 -0400

    MyActivity.java, main.xml * Changed the main view to show the new fragment * Removed draggable view adding code * Added action bar & menu option handling
    CanvasFragment.java, canvas.xml
    * Created this fragment for the canvas/drawing area
    * Moved main.xml code to the canvas.xml
    
    menu.xml, *.png
    * Added menu & icons
    
    .gitignore - Ignore files

--- src/com/colabug/viewdragger/MyActivity.java
+++ src/com/colabug/viewdragger/MyActivity.java
@@ -1,31 +1,62 @@
 package com.colabug.viewdragger;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MyActivity extends Activity
 {
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate( Bundle savedInstanceState )
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
 
         // Set view
-        setContentView( R.layout.main);
+        setContentView( R.layout.main );
 
-        // Create new layout parameters
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.mainLayout);
+        // Create an action bar
+        ActionBar bar = getActionBar();
+        bar.setDisplayOptions( ActionBar.DISPLAY_USE_LOGO |
+                               ActionBar.DISPLAY_SHOW_TITLE );
+        bar.setDisplayShowHomeEnabled( true );
+    }
 
-        // Create draggable views
-        DraggableView draggableView = new DraggableView(this);
-        draggableView.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.icon));
-        DraggableView draggableView2 = new DraggableView(this);
-        draggableView2.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.icon));
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate( R.menu.menu, menu );
+        return true;
+    }
 
-        // Add them to view
-        frameLayout.addView(draggableView);
-        frameLayout.addView(draggableView2);
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
+        CanvasFragment canvas = getCanvasFragment();
+        switch ( item.getItemId() )
+        {
+            // TODO: Undo last action
+            case R.id.undo:
+                return true;
+
+            case R.id.add:
+                canvas.addAnIcon();
+                return true;
+
+            case R.id.clear:
+                canvas.clearScreen();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected( item );
+        }
+    }
+
+    private CanvasFragment getCanvasFragment()
+    {
+        return (CanvasFragment) getFragmentManager().findFragmentById( R.id.canvas_fragment );
     }
 }
